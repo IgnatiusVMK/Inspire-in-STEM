3e4fa87417c071b2fda64fd36b2d912ff7c252bb
IgnatiusVMK <ignatiusvmk@gnmail.com>
2022-05-24 10:28:21 +0300

    Updates

--- assignment4.py
+++ assignment4.py
@@ -13,8 +13,8 @@ acc_bal = input("Enter your account balance ")
 if (int(acc_bal > 100000)) and (int(acc_bal <200000)):
     acc_bal = acc_bal -25000
     print("Ksh 25,000 has been debitted from your account.")
-else if (int(acc_bal) > 500000 and int(acc_bal <1000000)):
+elif (int(acc_bal) > 500000 and int(acc_bal <1000000)):
     acc_bal = acc_bal - acc_bal*0.3
     print(" We have deducted 30 percent from your accoungt")
-else if int(acc_bal > 1000000):
+elif int(acc_bal > 1000000):
     print("We have deducted Ksh 15,000 from your aacount.")
