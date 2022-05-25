bcd6f2aa0a0c3622ce0743b71d27ab54333aa0c8
IgnatiusVMK <ignatiusvmk@gmail.com>
2022-05-25 09:33:45 +0300

    Day 3

--- assignment4.py
+++ assignment4.py
@@ -10,11 +10,13 @@
 #   30% if acc is 500,000 1,000,000.
 
 acc_bal = input("Enter your account balance ")
-if (int(acc_bal) > 100000 and int(acc_bal < 200000)):
-    acc_bal = acc_bal -25000
-    print("Ksh 25,000 has been debitted from your account.")
-elif (int(acc_bal) > 500000 and int(acc_bal < 1000000)):
-    acc_bal = acc_bal - acc_bal*0.3
+if (int(acc_bal) > 100000) and (int(acc_bal) < 200000):
+    acc_bal = float(acc_bal) - 25000
+    print("Ksh 25,000 has been debitted from your account.\n")
+    print("Your balance is " + str(acc_bal))
+elif (int(acc_bal) > 500000) and (int(acc_bal) < 1000000):
+    acc_bal = float(acc_bal) - (0.3*float(acc_bal))
     print(" We have deducted 30 percent from your accoungt")
+    print("Your balance is " + str(acc_bal))
 elif int(acc_bal > 1000000):
     print("We have deducted Ksh 15,000 from your aacount.")
---
+++ assignment5.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+
+########################
+#          Assignment5
+#          Name: 
+#          Date: 24/5/2021
+########################
+
+#list of vehicles
+vehicles = ['bmw','audi','toyota','mercedes','jeep']
+
+# using a for loop to print a vehicle
+for vehicle in vehicles:
+    #print(vehicle.title())
+    if vehicle == "jeep":
+         print(vehicle.upper())
